6bf6ddb1db5bd044fa079d3ab026eaba22ac2168
TULIO DE LIMA AVELINO <tuhtuh199633@gmail.com>
2024-11-25 03:10:21 -0300

    implentação da classe usuario com getters,setters,toString(),equals() e hashCode()

--- modelo/Usuario.java
+++ modelo/Usuario.java
@@ -1,7 +1,40 @@
 package modelo;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Usuario {
+    private Integer id;
+    private String nome;
+    private String username;
+    private String email;
+    private String senha;
+    private LocalDateTime dataCadastro;
+    private List<Usuario> amigos = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
+
+    public Usuario(String nome, String username, String email, String senha) {
+        this.nome = nome;
+        this.username = username;
+        this.email = email;
+        this.senha = senha;
+        this.dataCadastro = LocalDateTime.now();
+    }
+
+    // Getters, setters, toString(), equals() e hashCode()
+
+    public void adicionarAmigo(Usuario amigo) {
+        amigos.add(amigo);
+    }
+
+    public void removerAmigo(Usuario amigo) {
+        amigos.remove(amigo);
+    }
+
+    public void adicionarPost(Post post) {
+        posts.add(post);
+    }
 }
+
