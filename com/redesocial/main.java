8d38b2671d03660a67c8b7dee4467ef66fd11bb0
TULIO DE LIMA AVELINO <tuhtuh199633@gmail.com>
2024-11-25 02:25:18 -0300

    desenvolvendo classe main

---
+++ com/redesocial/main.java
@@ -0,0 +1,10 @@
+package com.redesocial;
+
+import com.redesocial.ui.MenuPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.exibirMenu();
+    }
+}
